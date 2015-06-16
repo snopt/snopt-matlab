718648c5665f78176e1efd8f20c3ae42fc681315
Elizabeth Wong <elswong@gmail.com>
2015-06-16 14:57:45 -0700

    Add hs116

--- matlab/setpath.m
+++ matlab/setpath.m
@@ -7,4 +7,5 @@ addpath([pwd,'/examples/snmain'], '-end');
 addpath([pwd,'/examples/hsmain'], '-end');
 addpath([pwd,'/examples/hs76'  ], '-end');
 addpath([pwd,'/examples/fmincon'], '-end');
+addpath([pwd,'/examples/hs116'  ], '-end');
 
