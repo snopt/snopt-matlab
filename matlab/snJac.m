a7c2bc567dbd1b87265be9c111b889c7bf860201
Elizabeth Wong <elswong@gmail.com>
2016-06-22 23:03:50 -0700

    Added check for userfun.
    userfun can be string or function handle.

--- matlab/snJac.m
+++ matlab/snJac.m
@@ -1,7 +1,12 @@
-function  [A,iAfun,jAvar,iGfun,jGvar] = snJac(usrfun,x0,xlow,xupp,nF)
+function  [A,iAfun,jAvar,iGfun,jGvar] = snJac(userfun,x0,xlow,xupp,nF)
 %function [A,iAfun,jAvar,iGfun,jGvar] = snJac(usrfun,x0,xlow,xupp,nF)
 %         Finds the coordinate structure for the Jacobian.
 
 findJacOption = 17;
 
-[A,iAfun,jAvar,iGfun,jGvar] = snoptmex(findJacOption,usrfun,x0,xlow,xupp,nF);
+if (ischar(userfun))
+  userFG = str2func(userfun);
+else
+  userFG = userfun;
+end
+[A,iAfun,jAvar,iGfun,jGvar] = snoptmex(findJacOption,userFG,x0,xlow,xupp,nF);
