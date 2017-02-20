e520fc26ec38ca79545ceb41d18ea538a21e0f31
Elizabeth Wong <elswong@gmail.com>
2017-02-19 17:03:43 -0800

    Use checkFun
    checkFun used to check function input

--- matlab/snJac.m
+++ matlab/snJac.m
@@ -4,9 +4,10 @@ function  [A,iAfun,jAvar,iGfun,jGvar] = snJac(userfun,x0,xlow,xupp,nF)
 
 findJacOption = 17;
 
-if (ischar(userfun))
-  userFG = str2func(userfun);
-else
-  userFG = userfun;
+userFG = checkFun(userfun,'SNOPT');
+
+if nargout(userFG) ~= 1,
+  error('SNOPT:InputArgs','%s should only compute the objective function',inputname(userfun));
 end
+
 [A,iAfun,jAvar,iGfun,jGvar] = snoptmex(findJacOption,userFG,x0,xlow,xupp,nF);
